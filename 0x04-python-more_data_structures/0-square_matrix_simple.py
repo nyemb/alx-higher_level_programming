f47c5984c0d9a43714c21637cb8978d49d5061db
valjacques Nyemb <nyvanyemb@gmail.com>
2022-12-07 22:14:12 +0100

    add variable

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [[x**2 for x in row] for row in matrix]
+    new_matrix = [[x**2 for x in row] for row in matrix]
+    return new_matrix
 
