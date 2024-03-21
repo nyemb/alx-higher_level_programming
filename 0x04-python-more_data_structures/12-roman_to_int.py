a421136945a0e088c5c532a8a7d0a9f661e051f9
valjacques Nyemb <nyvanyemb@gmail.com>
2024-03-22 00:48:36 +0100

    fix problem on roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
-
 def roman_to_int(roman_string):
-    if roman_string is None or type(roman_string) != str:
-        return 0
-    roman = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
-    result = 0
-    for j in range(len(roman_string)):
-        if j > 0 and roman[roman_string[j]] > roman[roman_string[j - 1]]:
-            result += roman[roman_string[j]] - 2 * roman[roman_string[j - 1]]
-        else:
-            result += roman[roman_string[j]]
-    return result
+    num_map = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    i = 0
+    total = 0
+    if isinstance(roman_string, str):
+        for i in range(len(roman_string) - 1):
+            if num_map[roman_string[i]] >= num_map[roman_string[i + 1]]:
+                total += num_map[roman_string[i]]
+            else:
+                total -= num_map[roman_string[i]]
+            i += 1
+        total += num_map[roman_string[i]]
+        return (total)
+    return (0)
